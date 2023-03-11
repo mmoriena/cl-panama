3ac6bdaef29f45d1c05b1d66ea46f54dc2616e33
matias <mmoriena@quilsoft.com>
2023-03-11 13:05:03 -0300

    [ADD] prueba docker propios

--- panama_default/__manifest__.py
+++ panama_default/__manifest__.py
@@ -141,7 +141,7 @@
 
     # list of images to use in the form 'name image-url'
     'docker-images': [
-        'odoo jobiols/odoo-jeo:11.0',
-        'postgres postgres:10.1-alpine',
+        'odoo matiasmoriena/odoo-junio:3',
+        'postgres postgres:11',
     ]
 }
