920ab418de7aa64992f8c833f8448da39e3518b6
matias <mmoriena@quilsoft.com>
2023-03-12 10:53:38 -0300

    [ADD] pruebas

--- panama_default/__manifest__.py
+++ panama_default/__manifest__.py
@@ -99,33 +99,15 @@
             'web_responsive',
 
         ],
-    'installable': True,
 
     'env-ver': '2',
     'odoo-license': 'CE',
     'config': [
 
-        # 'addons_path' is always computed looking for the repositories in sources
-        # 'data_dir' is a fixed location inside docker odoo image
-
-        # You should use 2 worker threads + 1 cron thread per available CPU,
-        # and 1 CPU per 10 concurent users.
-        # if ommited oe will calculate workers and cron´s based on # of cpu
                 'workers = 0',
                 'max_cron_threads = 1',
-
-        # Number of requests a worker will process before being recycled and
-        # restarted. Defaults to 8192 if ommited
                 'limit_request = 8192',
-
-        # Maximum allowed virtual memory per worker. If the limit is exceeded,
-        # the worker is killed and recycled at the end of the current request.
-        # Defaults to 640MB
                 'limit_memory_soft = 2147483648',
-
-        # Hard limit on virtual memory, any worker exceeding the limit will be
-        # immediately killed without waiting for the end of the current request
-        # processing. Defaults to 768MB.
                 'limit_memory_hard = 2684354560',
     ],
 
@@ -140,8 +122,13 @@
     ],
 
     # list of images to use in the form 'name image-url'
-    'docker-images': [
-        'odoo matiasmoriena/odoo-junio:3',
-        'db_postgres postgres:11',
-    ]
+    "docker-images": [
+        "odoo jobiols/odoo-jeo:11.0",
+        "postgres postgres:10.1-alpine",
+        "aeroo jobiols/aeroo-docs",
+        "nginx nginx",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
 }
